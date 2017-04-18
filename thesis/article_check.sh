bde1fea8218ae2d0e88c04042555e42483f9013b
Paolo Di Giglio <p.digiglio91@gmail.com>
2017-04-18 23:26:20 +0200

    Add script to check article mistakes

---
+++ thesis/article_check.sh
@@ -0,0 +1,19 @@
+#!/usr/sh
+
+grep --color=auto -R "The chapter~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the chapter~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The section~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the section~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The figure~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the figure~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The equation~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the equation~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The table~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the table~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The listing~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the listing~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The code~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the code~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "The~" mainmatter/ backmatter/ frontmatter/
+grep --color=auto -R "the~" mainmatter/ backmatter/ frontmatter/
+
